e5a6d0e82799bc241d64e23003370d2ff9eda276
mwxgaf <mwxgaf@gmail.com>
2020-07-31 18:34:15 +0430

    Reduced code of test files

--- templates/test/test.php
+++ templates/test/test.php
@@ -1,16 +1,7 @@
 <!-- In the name of Allah -->
-<!DOCTYPE html>
-<html>
 
-<head>
-    <title>Test</title>
-</head>
-
-<body>
-    Done!
-    <br>
-    <?php echo "Seems that PyLocalhost is serving PHP files well :)" ?>
-    <br>
-    <b>You can delete this file if you would like</b>
-</body>
-</html>
+Done!
+<br>
+<?php echo "Seems that PyLocalhost is serving PHP files well :)" ?>
+<br>
+<b>You can delete this file if you would like</b>
